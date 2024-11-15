30fd890d90b85b0efbc8335c6ef936a379c41b5c
Lisa Berg <lisa.bergmohn@gmail.com>
2024-11-15 17:19:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const searchForm = document.getElementById("search-form");
+const inputVal = document.getElementById("input-value");
+const result = document.getElementById("result");
+const apiKey = "41059430";
+let movie = "";
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  movie = inputVal.value;
+
+  if (movie === "") {
+      result.innerHTML = "Enter movie title!";
+  } else {
+    getData(movie);
+  }
+});
+
+async function getData(movie) {
+  const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${movie}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    const movieItem = json.Search;
+
+    if(!movieItem || movieItem === 0) {
+      result.innerHTML = `No results found for ${movie}!`;
+      return;
+    }
+
+    fetchMovie(movieItem);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function fetchMovie(movieItem) {
+  result.innerHTML = "";
+    movieItem.forEach((item) => {
+      const para = document.createElement("p");
+      para.textContent = `🟡 ${item.Title}: (${item.Year})`;
+      result.appendChild(para);
+    });
+}
